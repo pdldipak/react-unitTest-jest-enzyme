973162939d1e531baaf687e953bc9adff1863780
Dipak Poudel <pdl.dipak@gmail.com>
2021-07-07 11:52:09 +0200

    initial setup for unit test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Counter from './counter/Counter';
+
+function App() {
+  return (
+    <div className='App' testID='component-app'>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
